194db457dde0cfb7ade2e99f108bc342193d1edf
Max Dawson <maxjdawson7@gmail.com>
2024-10-31 16:17:17 +0000

    Added a paragraph to index.php displaying the user's IP address. Not impemented in github public version

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <body>
     <p>WebWorks RapidServe</p>
     <p>When using you accept our <a href="/terms.php">terms</a>.</p>
+    <p>Your IP <?php echo $_SERVER['REMOTE_ADDR']; ?> may be logged for security reasons.</p>
 
     <a href="/manifest.php">Manifest</a>
 </body>
